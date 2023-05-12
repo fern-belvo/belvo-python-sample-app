287e430e94fd6d9cf6eecb3a935d3a15a898e525
Zach Kirsch <zachkirsch@gmail.com>
2023-05-12 13:00:37 -0700

    Add sample app

---
+++ src/belvo_python_sample_app/main.py
@@ -0,0 +1,25 @@
+import belvo
+from belvo.client import Belvo
+
+
+def main() -> None:
+    belvo_client = Belvo(
+        secret_id="YOUR_SECRET_ID",
+        secret_password="YOUR_SECRET_PASSWORD",
+    )
+
+    link = belvo_client.links.register_link(
+        body=belvo.LinksRequest(
+            institution="banamex_mx_retail",
+            username="username",
+            password="password",
+            accessMode=belvo.EnumLinkAccessModeRequest.SINGLE,
+            credentialsStorage="30d",
+        )
+    )
+
+    print(link)
+
+
+if __name__ == "__main__":
+    main()
